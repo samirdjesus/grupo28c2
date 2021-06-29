b37c121012b81b5a1d8219e0569468a0c7e2291d
samirdjesus <samirdejesus@gmail.com>
2021-06-29 17:07:18 -0500

    second commit

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App {
+    private String nombre;
+    private int edad;
+    private double estatura;
+    private float peso;
+    private char inicial;
+    private String direccionVivienda;
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World");
+    }
+}
